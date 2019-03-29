353bdfc54e3cf33af1fc8187a6463c14a5f7c787
dharm18 <vishdharm.181910@gmail.com>
2019-03-29 20:41:08 +0000

    test

--- Lab1-intro.R
+++ Lab1-intro.R
@@ -7,3 +7,23 @@ table(y)
 #explore the data
 prop.table(table(y))
 barplot(table(y), main = "Distribution of Titanic Survival", ylab="Frequency")
+
+set.seed(1337)
+index <- sample(1:length(y), length(y) * .25, replace = F)
+testing <- y[index]
+
+perishModel <- rep("No", length(testing))
+
+coinModel <- round(runif(length(testing), min=0, max=1))
+coinModel <- factor(coinModel, levels = c(0,1), labels = c("No", "Yes"))
+
+perishModel <- factor(perishModel, levels = c("No", "Yes"), labels = c("No", "Yes"))
+
+table(testing, perishModel)
+
+table(testing, coinModel)
+
+# Determining Prediction Accuracy
+
+(coinAccuracy <- 1 - mean(coinModel != testing))
+(perishAccuracy <- 1 - mean(perishModel != testing))
